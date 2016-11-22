47b2c88bc837098f1c11d35d0171737ec6ac5205
R45H <gmr45h@gmail.com>
2016-11-22 12:27:13 +0300

    Add the font awesome. Changed the footer

--- app/libs/bootstrap/package.js
+++ app/libs/bootstrap/package.js
@@ -14,11 +14,11 @@ Package.onUse(function (api) {
   api.versionsFrom('METEOR@1.0');
   api.use('jquery', 'client');
   var assets = [
-    'dist/fonts/glyphicons-halflings-regular.eot',
-    'dist/fonts/glyphicons-halflings-regular.svg',
-    'dist/fonts/glyphicons-halflings-regular.ttf',
-    'dist/fonts/glyphicons-halflings-regular.woff',
-    'dist/fonts/glyphicons-halflings-regular.woff2'
+    'dist/font-awesome/glyphicons-halflings-regular.eot',
+    'dist/font-awesome/glyphicons-halflings-regular.svg',
+    'dist/font-awesome/glyphicons-halflings-regular.ttf',
+    'dist/font-awesome/glyphicons-halflings-regular.woff',
+    'dist/font-awesome/glyphicons-halflings-regular.woff2'
   ];
   if (api.addAssets) {
     api.addAssets(assets, 'client');
