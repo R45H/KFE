9552aa9375c8c2eb0ffb97fbabbee91dbb3ea182
R45H <gmr45h@gmail.com>
2016-11-29 17:54:59 +0300

    Complete the scroll pagination. Remove border radius from the safari.

--- app/src/blocks/_js-scroll/_js-scroll.js
+++ app/src/blocks/_js-scroll/_js-scroll.js
@@ -2,24 +2,36 @@ $(function() {
 
 	$.scrollify({
 		section : ".js-scroll",
-		sectionName: false,
+		sectionName: "scroll",
 		scrollSpeed: 700,
-		setHeights: false
+		setHeights: false,
+
+		before: function (i, panels) {
+			var ref = panels[i].attr("data-scroll");
+			$(".pag__link_active").removeClass("pag__link_active");
+			$(".header__pag").find('a[href="#' + ref + '"]').addClass("pag__link_active");
+		},
+
+		afterResize: function() {
+			if (window.innerWidth < 425) {
+				$.scrollify.disable();
+			} else {
+				$.scrollify.enable();
+			}
+		},
+
+		afterRender: function() {
+			if (window.innerWidth < 425) {
+				$.scrollify.disable();
+			} else {
+				$.scrollify.enable();
+			}
+		}
 	});
+
+	$(".pag__link").on("click", $.scrollify.move);
 
 	// $.scrollify.disable();
 
-	if (window.innerWidth < 425) {
-		$.scrollify.disable();
-	} else {
-		$.scrollify.enable();
-	}
-
-	$(window).on('resize', function() {
-		if (window.innerWidth < 425) {
-			$.scrollify.disable();
-		} else {
-			$.scrollify.enable();
-		}
-	});
 });
+
