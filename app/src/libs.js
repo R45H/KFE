aed49d1a2624fcec7a4e0bda52527928d4b38ef3
Олег <gmr45h@gmail.com>
2017-02-17 11:22:31 +0300

    Add jquery-ui

--- app/src/libs.js
+++ app/src/libs.js
@@ -1,3 +1,4 @@
 @@include('../libs/jquery/dist/jquery.js')
 @@include('../libs/bootstrap/dist/js/bootstrap.js')
 @@include('../libs/owl.carousel/dist/owl.carousel.js')
+@@include('../libs/jquery-ui/jquery-ui.js')
