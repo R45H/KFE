5ec3d3dd522e8315b750ba4ffd0a75a153f4c0ee
R45H <gmr45h@gmail.com>
2017-02-27 13:05:37 +0300

    Complete 6-th section. Add tabs

---
+++ app/src/blocks/commend/_commend.js
@@ -0,0 +1,43 @@
+$(function() {
+	$('.commend__link').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+	$('.commend__item_next').on('click', function() {
+		$('.commend__item.active')
+			.next()
+			.find('.commend__link')
+			.tab('show')
+		;
+	});
+
+	$('.commend__item_prev').on('click', function() {
+		$('.commend__item.active')
+			.prev()
+			.find('.commend__link')
+			.tab('show')
+		;
+	});
+
+	if (window.innerWidth < 768) {
+		checkTabs();
+	}
+
+	$(window).on('resize', function() {
+		checkTabs();
+	});
+
+	$('.commend__link').on('shown.bs.tab', function() {
+		checkTabs();
+	});
+
+	function checkTabs() {
+		if (window.innerWidth < 768) {
+			$('.commend__item:not(.active):not(.commend__item_prev):not(.commend__item_next)').hide();
+			$('.commend__item.active').show();
+		} else {
+			$('.commend__item:not(.commend__item_prev):not(.commend__item_next)').show();
+		}
+	}
+});
