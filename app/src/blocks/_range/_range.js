6983a74ead6acee5db79bc28120059cb54e32036
alexandr <al.rout@yandex.ru>
2017-02-28 16:43:19 +0300

    add range slider

--- app/src/blocks/_range/_range.js
+++ app/src/blocks/_range/_range.js
@@ -1,32 +1,74 @@
-$(function() {
+$(function () {
 
-	$('#range-1').slider({ // Ползунки выбора диапазона
-		range: true,
-		min: 24,
-		max: 10805,
-		values: [1000, 10000],
-		create: function() {
-			$(this).attr('data-min', $(this).slider('values', 0));
-			$(this).attr('data-max', $(this).slider('values', 1));
-		},
-		change: function() {
-			$(this).attr('data-min', $(this).slider('values', 0));
-			$(this).attr('data-max', $(this).slider('values', 1));
-		}
-	});
-
-	$('#range-2').slider({
-		range: true,
+	jQuery("#slider").slider({
 		min: 0,
-		max: 250,
-		values: [0, 250],
-		create: function() {
-			$(this).attr('data-min', $(this).slider('values', 0));
-			$(this).attr('data-max', $(this).slider('values', 1));
+		max: 1000,
+		values: [0, 1000],
+		range: true,
+		create: function (event, ui) {
+			changeInput();
 		},
-		change: function() {
-			$(this).attr('data-min', $(this).slider('values', 0));
-			$(this).attr('data-max', $(this).slider('values', 1));
+		stop: function (event, ui) {
+			jQuery("input#minCost").val(jQuery("#slider").slider("values", 0));
+			jQuery("input#maxCost").val(jQuery("#slider").slider("values", 1));
+			changeInput();
+
+		},
+		slide: function (event, ui) {
+			jQuery("input#minCost").val(jQuery("#slider").slider("values", 0));
+			jQuery("input#maxCost").val(jQuery("#slider").slider("values", 1));
+			changeInput();
 		}
 	});
+
+	jQuery("input#minCost").on('change keydown', function () {
+		var value1 = jQuery("input#minCost").val();
+		var value2 = jQuery("input#maxCost").val();
+
+		if (parseInt(value1) > parseInt(value2)) {
+			value1 = value2;
+			jQuery("input#minCost").val(value1);
+		}
+		jQuery("#slider").slider("values", 0, value1);
+		changeInput();
+	});
+
+	jQuery("input#maxCost").on('change keydown', function () {
+		var value1 = jQuery("input#minCost").val();
+		var value2 = jQuery("input#maxCost").val();
+
+		if (value2 > 1000) {
+			value2 = 1000;
+			jQuery("input#maxCost").val(1000)
+		}
+
+		if (parseInt(value1) > parseInt(value2)) {
+			value2 = value1;
+			jQuery("input#maxCost").val(value2);
+		}
+		jQuery("#slider").slider("values", 1, value2);
+		changeInput();
+	});
+
+	function changeInput() {
+		inp = $("input[id='minCost']");
+		number = $("input[id='minCost']").val().length;
+		$("#count").html("Количество введенных символов: " + number);
+		if (window.innerWidth >= 1400) {
+			var q = 21.08 * number;
+		} else {
+			var q = 13.17 * number;
+		}
+		$(inp).css('width', q);
+
+		inp2 = $("input[id='maxCost']");
+		number2 = $("input[id='maxCost']").val().length;
+		if (window.innerWidth >= 1400) {
+			var s = 21.08 * number2;
+		} else {
+			var s = 13.17 * number2;
+		}
+		$(inp2).css('width', s);
+	}
+
 });
