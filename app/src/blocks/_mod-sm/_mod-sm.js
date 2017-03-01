16cda7770ce8233974327257d9ce39c1809e983b
R45H <gmr45h@gmail.com>
2017-03-02 01:32:49 +0300

    Create small modal windows

---
+++ app/src/blocks/_mod-sm/_mod-sm.js
@@ -0,0 +1,16 @@
+$(function() {
+	var
+		$modal = $('.mod-sm'), // Модальное окно
+		$header = $('.header'), // Шапка
+		$up = $('.up'); // Кнопка "наверх"
+
+	$modal.on('show.bs.modal', function() {
+		$header.css('padding-right', '17px');
+		$up.css('right', '17px');
+	});
+
+	$modal.on('hidden.bs.modal', function() {
+		$header.css('padding-right', '0');
+		$up.css('right', '0');
+	});
+});
