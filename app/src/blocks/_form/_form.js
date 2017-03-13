fa63513267bfd73893beaa753813ddfb43b597cf
Олег <gmr45h@gmail.com>
2017-03-13 17:48:38 +0300

    Create a new page best-offers

---
+++ app/src/blocks/_form/_form.js
@@ -0,0 +1,8 @@
+/* Работа переключателя дополнительных параметров поиска */
+$(function() {
+	$('.form__add-btn').on('click', function() {
+		$('.form__add').slideToggle(300);
+		return false;
+	});
+});
+/* ========== */
