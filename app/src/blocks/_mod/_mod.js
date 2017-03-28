8a2b59ee534f8f20f7db5987ba618ca9da43b75f
Олег <gmr45h@gmail.com>
2017-03-28 18:12:59 +0300

    Add font Tahoma. New fixes

--- app/src/blocks/_mod/_mod.js
+++ app/src/blocks/_mod/_mod.js
@@ -3,12 +3,14 @@ $(function() {
 	var
 		$modal = $('.mod'), // Модальное окно
 		$header = $('.header'), // Шапка
+		$menuI = $('.l-menu-i'), // Шапка
 		$up = $('.up'), // Кнопка "наверх"
 		$input = 'input', // Класс инпута
 		$inputFillClass = 'input_fill'; // Класс заполненного инпута
 
 	$modal.on('show.bs.modal', function() {
 		$header.css('padding-right', '17px');
+		$menuI.css('padding-right', '17px');
 		$up.css('right', '17px');
 	});
 
@@ -18,6 +20,7 @@ $(function() {
 
 	$modal.on('hidden.bs.modal', function() {
 		$header.css('padding-right', '0');
+		$menuI.css('padding-right', '0');
 		$up.css('right', '0');
 		$(this) // Очистка инпутов
 			.find('.' + $input)
