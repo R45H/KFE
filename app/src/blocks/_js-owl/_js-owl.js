cc714ec03ab355b1b021c052bf529a01d009d357
Олег <gmr45h@gmail.com>
2017-04-11 17:49:31 +0300

    Make new slider for the object page

--- app/src/blocks/_js-owl/_js-owl.js
+++ app/src/blocks/_js-owl/_js-owl.js
@@ -9,5 +9,17 @@ $(document).ready(function(){
 	});
 
 	$('.js-owl .owl-dots').wrap('<div class="container js-owl__dots"></div>');
+
+	$('.js-owl_half').each(function() {
+		var $this = $(this);
+
+		$this
+			.find('.js-owl__dots')
+			.removeClass('container');
+
+		$this
+			.find('.owl-nav')
+			.insertAfter($this);
+	});
 });
 /* ========== */
